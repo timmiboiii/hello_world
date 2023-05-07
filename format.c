70847481e0d13f8fe42cdffcbde703ca2c2a472d
Kai <kaiquinders@gmail.com>
2023-05-07 17:21:57 +0200

    added format.c

---
+++ format.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+	int ganzzahl = 25;
+	float gleitkommazahl = 2.5;
+	char zeichen = 'X';
+	char zeichenkette [13] = "Hello World!";
+
+	printf("Eine Ganzzahl: %i, Eine Gleitkommazahl: %.1f, ein Zeichen:\"%c\" und eine Zeichenkette \"%s\".\n", ganzzahl, gleitkommazahl, zeichen, zeichenkette);
+	return 0;
+
+}
