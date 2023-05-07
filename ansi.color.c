20fdb96d36ffaa84bba6bb001c01d6c76a8e0c09
Timmiboi <tibremser93@googlemail.com>
2023-05-08 00:23:43 +0200

    Zeichnung eines gefärbten Baumes.

---
+++ ansi.color.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+
+
+	printf("\033[0;32m        _-_\n");
+	printf("     /~~   ~~\\ \n ");
+	printf(" /~~         ~~\\ \n ");
+	printf("\{               } \n ");
+	printf(" \\  _-     -_  /\033[0m\n ");
+	printf("\033[0;33m    ~ \\\\ //  ~\n");
+	printf(" _- -   | | _- _\n");
+	printf("   _ -  | |   -_\n");
+	printf("       // \\\\ \033[0m \n");
+
+
+}
